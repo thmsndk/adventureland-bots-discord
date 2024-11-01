722d9f2cd46c27b38be89fb4139b61850cc0558a
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-11-01 08:15:07 +0100

    poc of styled embeds

--- source/tests/discord/bot.ts
+++ source/tests/discord/bot.ts
@@ -3,7 +3,7 @@ import { Client, IntentsBitField } from "discord.js"
 import fs from "fs"
 import interactionCreate from "./interactionCreate.js"
 import ready from "./listeners/ready.js"
-import { initializeMerchantTracker } from "./merchantTracker.js"
+import { initializeMerchantTracker, testInitializeMerchantTracker } from "./merchantTracker.js"
 
 const credentials = JSON.parse(fs.readFileSync("../../../credentials.json", "utf-8"))
 await AL.Game.getGData(true, false)
@@ -12,12 +12,18 @@ await AL.Game.loginJSONFile("../../../credentials.json")
 console.log("Bot is starting...")
 
 const client = new Client({
-    intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent]
+    intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent],
 })
+// const client = new Client({
+//     intents: [IntentsBitField.Flags.MessageContent],
+// })
 
 ready(client)
-interactionCreate(client)
+// interactionCreate(client)
 
 // Initialize the merchant tracking with the client instance
-initializeMerchantTracker(client, "1079859790831951914") // Pass the trade channel ID
+// initializeMerchantTracker(client, "1079859790831951914") // Pass the trade channel ID
+testInitializeMerchantTracker(client, "1079859790831951914")
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+void client.login(credentials.discord.auth)
